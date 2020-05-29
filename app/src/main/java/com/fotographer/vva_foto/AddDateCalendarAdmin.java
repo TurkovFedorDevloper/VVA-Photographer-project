388f2c7df5e619f881456f5fabe267946b1d1353
Turkov Fedor <TurkovDevelopre@mail.ru>
2020-05-30 01:58:51 +0300

    changes

---
+++ app/src/main/java/com/fotographer/vva_foto/AddDateCalendarAdmin.java
@@ -0,0 +1,14 @@
+package com.fotographer.vva_foto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddDateCalendarAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_date_calendar_admin);
+    }
+}
